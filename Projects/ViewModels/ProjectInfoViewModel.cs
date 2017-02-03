675b7443989f084a43dcc006da7fb44db6b00ec3
Pietro Simonini <pietro.simonini@hotmail.it>
2017-02-03 12:28:17 +0000

    Added file ProjectInfoViewModel.cs

---
+++ Projects/ViewModels/ProjectInfoViewModel.cs
@@ -0,0 +1,27 @@
+using DBManager;
+using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects.ViewModels
+{
+    internal class ProjectInfoViewModel : BindableBase
+    {
+        private DBEntities _entities;
+        private EventAggregator _eventAggregator;
+        private Project _projectInstance;
+
+        internal ProjectInfoViewModel(DBEntities entities, 
+                                    EventAggregator aggregator, 
+                                    Project instance) 
+            : base()
+        {
+            
+        }
+        
+}
