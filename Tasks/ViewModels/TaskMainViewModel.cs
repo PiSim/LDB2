1bd77aa95570d7a614ecc1f60a4cd9a7e5bbf506
Pietro Simonini <pietro.simonini@hotmail.it>
2017-02-03 11:57:14 +0000

    Updated TaskMainViewModel.cs

--- Tasks/ViewModels/TaskMainViewModel.cs
+++ Tasks/ViewModels/TaskMainViewModel.cs
@@ -2,7 +2,7 @@
 using Prism.Events;
 using Prism.Mvvm;
 using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +13,19 @@ namespace Tasks.ViewModels
     {
         private DBEntities _entities;
         private EventAggregator _eventAggregator;
+        private ObservableCollection<Task> _taskList;
 
         internal TaskMainViewModel(DBEntities entities, EventAggregator eventAggregator) 
             : base()
         {
             _entities = entities;
             _eventAggregator = eventAggregator;
+            _taskList = new ObservableCollection(_entities.Tasks);
+        }
+        
+        public ObservableCollection<Task> TaskList
+        {
+            get { return _taskList; }
         }
     }
 }
