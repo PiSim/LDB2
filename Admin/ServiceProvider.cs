c4133a41a02b0e4173a3a53095a14cbda2663231
Pietro Simonini <pietro.simonini@hotmail.it>
2017-02-07 09:18:12 +0000

    Updated ServiceProvider.cs

--- Admin/ServiceProvider.cs
+++ Admin/ServiceProvider.cs
@@ -1,14 +1,33 @@
 using DBManager;
-
-
-
+using System;
 
 
 public class ServiceProvider
 {
     private DBEntities _entities;
     
-    public static
+    public ServiceProvider(DBEntities entities)
+    {
+        _entities = entities;
+    }
     
-    public void 
+    public void BuildExternalReportFiles()
+    {
+        foreach (ExternalReport ext in _entities.ExternalReports)
+        {
+            string[] fileIDs = ext.fileID.Split((char)007);
+            
+            foreach (string id in fileIDs)
+            {
+                File tempFile = _entities.Files[int.Parse(id)];
+                
+                ExternalReportFile tempExt = new ExternalReportFile();
+                tempExt.ExternalReport = ext;
+                tempExt.Path = tempFile.Path;
+                tempExt.Description = tempFile.Description;                
+            }
+        }
+        
+        _entities.SaveChanges();
+    }
 }
