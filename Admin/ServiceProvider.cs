c890e8f3ed05bc5c1f0997fd8366a9bc6ed95dd2
Pietro Simonini <pietro.simonini@hotmail.it>
2017-02-09 13:46:26 +0000

    Updated ServiceProvider.cs

--- Admin/ServiceProvider.cs
+++ Admin/ServiceProvider.cs
@@ -13,5 +13,22 @@ public class ServiceProvider
         _entities = entities;
     }
     
+    public void BuildMeasurements()
+    {
+        foreach (Method mtd in _entities.Methods)
+        {
+            string[] _meaList = mtd.Measurements.Split((char)007);
+            foreach (string mea in _meaList)
+            {
+                Measurement tempMea = new Measurement();
+                tempMea.Name = mea;
+                tempMea.UM = mtd.UM;
+                mtd.Measurements.Add(tempMea);
+            }
+            
+        }
+        
+        _entities.SaveChanges();
+    }
 
 }
