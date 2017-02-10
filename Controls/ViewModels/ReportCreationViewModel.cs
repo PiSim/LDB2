571dd6829278a3d03098ac200387d0590bc4556e
Pietro Simonini <pietro.simonini@hotmail.it>
2017-02-10 15:50:28 +0000

    Updated ReportCreationViewModel.cs

--- Controls/ViewModels/ReportCreationViewModel.cs
+++ Controls/ViewModels/ReportCreationViewModel.cs
@@ -44,9 +44,7 @@ namespace Controls.ViewModels
                     
                     _entities.GenerateTestList(temp, 
                                             _requirementList.Where(req => req.IsSelected).Select(req => req.Instance));
-                    
-                    _entities.Reports.Add(temp);
-                    
+                                        
                     _parentDialog.ReportInstance = temp;
                     _parentDialog.DialogResult = true;
                 },
