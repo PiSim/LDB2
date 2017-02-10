525c018ab61688daca5f3558fa1f79592cc6d93e
Pietro Simonini <pietro.simonini@hotmail.it>
2017-02-10 08:51:38 +0000

    Updated BatchInfoViewModel.cs

--- Batches/ViewModels/BatchInfoViewModel.cs
+++ Batches/ViewModels/BatchInfoViewModel.cs
@@ -1,4 +1,9 @@
-﻿using DBManager;
+﻿using Controls;
+using DBManager;
+using Infrastructure;
+using Reports;
+using Prism.Commands;
+using Prism.Events;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
@@ -12,24 +17,64 @@ namespace Batches.ViewModels
     class BatchInfoViewModel : BindableBase
     {
         private Batch _instance;
+        private DelegateCommand _newReport, _openReport;
+        private EventAggregator _eventAggregator;
         private List<SamplesWrapper> _samplesList;
+        private Report _selectedReport;
+        private UnityContainer _container;
 
-        internal BatchInfoViewModel(Batch instance) : base()
+        internal BatchInfoViewModel(Batch instance,
+                                    EventAggregator aggregator,
+                                    UnityContainer container) : base()
         {
+            _container = container;
+            _eventAggregator = aggregator;
             _instance = instance;
             _samplesList = new List<SamplesWrapper>();
             foreach (Sample smp in _instance.Samples)
                 _samplesList.Add(new SamplesWrapper(smp));
+                
+            _newReport = new DelegateCommand(
+                () => 
+                {
+                    ReportCreationDialog reportDialog = _container.Resolve<ReportCreationDialog>();
+                    if (reportDialog.ShowDialog == true)
+                    {
+                        SelectedReport = reportDialog.ReportInstance;
+                        _openReport.Execute();
+                    }
+                    
+                }
+            );
+            
+            _openReport = new DelegateCommand(
+                () => 
+                {
+                    ObjectNavigationToken token = new ObjectNavigationToken(_selectedReport, Reports.ViewNames.ReportEditView);
+                    _eventAggregator.GetEvent<VisualizeObjectRequested>().Publish(token);
+                }
+            );
+                
         }
 
         public Material Material
         {
             get { return _instance.Material; }
         }
+        
+        public DelegateCommand NewReportCommand
+        {
+            get { return _newReport;}
+        }
 
         public string Number
         {
             get { return _instance.Number; }
+        }
+
+        public DelegateCommand OpenReportCommand
+        {
+            get { return _openReport; }
         }
 
         public Project Project
