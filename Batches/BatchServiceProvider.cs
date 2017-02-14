74fcac7f64d23bba32b99dd937f3294cefb35977
Pietro Simonini <pietro.simonini@hotmail.it>
2017-02-14 11:53:56 +0000

    Updated BatchServiceProvider.cs

--- Batches/BatchServiceProvider.cs
+++ Batches/BatchServiceProvider.cs
@@ -1,1 +1,25 @@
+using Controls.Views;
 using DBManager;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch
+{
+    public class BatchServiceProvider
+    {
+        
+        public BatchServiceProvider()
+        {
+            
+        }
+    
+    
+        
+    }
+    
+    
+}
