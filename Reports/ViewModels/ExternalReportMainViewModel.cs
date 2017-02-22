203db2569884f5af11af0db337f043494b9c4bff
Pietro Simonini <pietro.simonini@hotmail.it>
2017-02-22 13:30:17 +0000

    Updated ExternalReportMainViewModel.cs

--- Reports/ViewModels/ExternalReportMainViewModel.cs
+++ Reports/ViewModels/ExternalReportMainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using DBManager;
 using Infrastructure;
+using Microsoft.Practices.Unity;
 using Prism.Commands;
 using Prism.Events;
 using Prism.Mvvm;
@@ -17,11 +18,14 @@ namespace Reports.ViewModels
         private DelegateCommand _newReport, _openReport;
         private EventAggregator _eventAggregator;
         private ExternalReport _selectedReport;
-        private ObservableCollection<ExternalReport> _reportList;       
+        private ObservableCollection<ExternalReport> _reportList;      
+        private UnityContainer _container; 
 
         internal ExternalReportMainViewModel(DBEntities entities, 
-                                            EventAggregator aggregator)
+                                            EventAggregator aggregator,
+                                            UnityContainer container)
         {
+            _container = container;
             _entities = entities;
             _eventAggregator = aggregator;
             _reportList = new ObservableCollection<ExternalReport>(_entities.ExternalReports);
@@ -29,7 +33,14 @@ namespace Reports.ViewModels
             _newReport = new DelegateCommand(
                 () => 
                 {
-                    
+                    Views.ExternalReportCreationDialog creationDialog = _container.Resolve<Views.ExternalReportCreationDialog>();
+                    if (creationDialog.ShowDialog() == true)
+                    {
+                        ExternalReport tempReport = creationDialog.ExternalReportInstance;
+                        _reportList.Add(tempReport);
+                        SelectedReport = tempReport;
+                        _openReport.Execute();
+                    }
                 });
 
             _openReport = new DelegateCommand(
@@ -40,7 +51,8 @@ namespace Reports.ViewModels
 
                     _eventAggregator.GetEvent<Infrastructure.Events.VisualizeObjectRequested>()
                         .Publish(token);
-                });
+                },
+                () => _selectedReport != null);
         } 
         
         public DelegateCommand NewReportCommand
@@ -61,7 +73,11 @@ namespace Reports.ViewModels
         public ExternalReport SelectedReport
         {
             get { return _selectedReport; }
-            set { _selectedReport = value; }
+            set 
+            { 
+                _selectedReport = value; 
+                _openReport.RaiseCanExecuteChanged();
+            }
         }
     }
 }
