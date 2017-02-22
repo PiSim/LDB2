73146cb22c49a57e66ebddfd52396bc89a1d9ea8
Pietro Simonini <pietro.simonini@hotmail.it>
2017-02-22 08:18:00 +0000

    Updated BatchPickerViewModel.cs

--- Controls/ViewModels/BatchPickerViewModel.cs
+++ Controls/ViewModels/BatchPickerViewModel.cs
@@ -12,6 +12,8 @@ namespace Controls.ViewModels
     internal class BatchPickerViewModel : BindableBase
     {
         private DBEntities _entities;
+        private DelegateCommand _cancel, _confirm;
+        private string _number;
         private Views.BatchPickerDialog _parentDialog;
         
         internal BatchPickerViewModel(DBEntities entities,
@@ -19,9 +21,38 @@ namespace Controls.ViewModels
         {
             _entities = entities;
             _parentDialog = parentDialog;
+            
+            _cancel = new DelegateCommand(
+                () => 
+                {
+                    
+                });
+                
+            _confirm = new DelegateCommand(
+                () => 
+                {
+                    
+                });
         }
         
+        public DelegateCommand CancelCommand
+        {
+            get { return _cancel; }
+        }
         
+        public DelegateCommand ConfirmCommand
+        {
+            get { return _confirm; }
+        }
+        
+        public string Number
+        {
+            get { return _number; }
+            set 
+            {
+                _number = value;
+            }
+        }
         
     }
 }
