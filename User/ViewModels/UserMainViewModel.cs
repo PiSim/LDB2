93c80c8599ee5e58ab602c927ae25e223cc6d1ad
Pietro Simonini <pietro.simonini@hotmail.it>
2017-02-26 23:37:24 +0100

    base user view implemented

---
+++ User/ViewModels/UserMainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User.ViewModels
+{
+    internal class UserMainViewModel : BindableBase
+    {
+        internal UserMainViewModel() : base()
+        {
+
+        }
+    }
+}
