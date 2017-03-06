fa2e0f23d52acc0fb7cac6deba28ad6c02e371c8
Pietro Simonini <pietro.simonini@hotmail.it>
2017-03-06 13:50:12 +0000

    Added file RegionNames.cs

---
+++ Infrastructure/RegionNames.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class RegionNames
+    {
+        public static string MainNavigationRegion { get { return "MainNavigationRegion"; } }
+        public static string MainRegion { get { return "MainRegion"; } }
+        public static string MethodEditRegion { get { return "MethodEditRegion"; } }
+    }
+}
