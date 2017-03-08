7c120e4a72923845211f7fd803a14ca46ab863f9
Pietro Simonini <pietro.simonini@hotmail.it>
2017-03-08 12:22:25 +0000

    Updated MethodEditViewModel.cs

--- Specifications/ViewModels/MethodEditViewModel.cs
+++ Specifications/ViewModels/MethodEditViewModel.cs
@@ -5,6 +5,7 @@ using Prism.Events;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,8 +15,11 @@ namespace Specifications.ViewModels
     public class MethodEditViewModel : BindableBase
     {
         private DBEntities _entities;
+        private DelegateCommand _addMeasurement, _removeMeasurement;
         private EventAggregator _eventAggregator;
         private Method _methodInstance;
+        private MethodMeasurement _selectedMeasurement;
+        private ObservableCollection<MethodMeasurement>_measurementList;
 
         public MethodEditViewModel(DBEntities entities, EventAggregator aggregator) : base()
         {
@@ -23,6 +27,28 @@ namespace Specifications.ViewModels
             _eventAggregator = aggregator;
 
             _eventAggregator.GetEvent<CommitRequested>().Subscribe(() => _entities.SaveChanges());
+            
+            _addMeasurement = new DelegateCommand(
+                () => 
+                {
+                    MethodMeasurement tempMea = new MethodMeasurement();
+                    tempMea.Name = "Nuova Prova";
+                    tempMea.UM = "";
+                    Measurements.Add(tempMea);
+                });
+            
+            _removeMeasurement = new DelegateCommand(
+                () => 
+                {
+                    Measurements.Remove(SelectedMeasurement);
+                    SelectedMeasurement = null;
+                },
+                () => SelectedMeasurement != null );
+        }
+        
+        public DelegateCommand AddMeasurementCommand
+        {
+            get { return _addMeasurement; }
         }
 
         public Method MethodInstance
@@ -31,6 +57,22 @@ namespace Specifications.ViewModels
             set
             {
                 _methodInstance = value;
+                Measurements = new ObservableCollection<MethodMeasurement>(_methodInstance.Measurements);
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public ObservableCollection<MethodMeasurement> Measurements
+        {
+            get 
+            { 
+                return _measurementList;
+            }
+            
+            private set 
+            {
+                _measurementList = value;
+                OnPropertyChanged("Measurements");
             }
         }
 
@@ -41,5 +83,25 @@ namespace Specifications.ViewModels
                 return (_methodInstance != null) ? _methodInstance.Standard.Name : null;
             }
         }
+        
+        public DelegateCommand RemoveMeasurementCommand
+        {
+            get { return _removeMeasurement; }
+        }
+        
+        public MethodMeasurement SelectedMeasurement
+        {
+            get 
+            {
+                return _selectedMeasurement;
+            }
+            set
+            {
+                _selectedMeasurement = value;
+                _removeMeasurement.RaiseCanExecuteChanged();
+                OnPropertyChanged("SelectedMeasurement");
+            }
+        }
+        
     }
 }
