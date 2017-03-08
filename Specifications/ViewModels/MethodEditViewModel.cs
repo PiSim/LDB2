f7ec4479e5069ccc8c1e08d8d2ce8b5c89d8dca1
Pietro Simonini <pietro.simonini@hotmail.it>
2017-03-08 23:37:41 +0100

    Improved MethodEditView

--- Specifications/ViewModels/MethodEditViewModel.cs
+++ Specifications/ViewModels/MethodEditViewModel.cs
@@ -6,6 +6,7 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Forms;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,14 @@ namespace Specifications.ViewModels
     public class MethodEditViewModel : BindableBase
     {
         private DBEntities _entities;
-        private DelegateCommand _addMeasurement, _removeMeasurement;
+        private DelegateCommand _addFile, _addIssue, _addMeasurement, _openFile, _removeFile, _removeIssue, _removeMeasurement;
         private EventAggregator _eventAggregator;
         private Method _methodInstance;
         private MethodMeasurement _selectedMeasurement;
         private ObservableCollection<MethodMeasurement>_measurementList;
+        private ObservableCollection<StandardIssue> _issueList;
+        private StandardIssue _selectedIssue;
+        private StandardFile _selectedFile;
 
         public MethodEditViewModel(DBEntities entities, EventAggregator aggregator) : base()
         {
@@ -27,7 +31,43 @@ namespace Specifications.ViewModels
             _eventAggregator = aggregator;
 
             _eventAggregator.GetEvent<CommitRequested>().Subscribe(() => _entities.SaveChanges());
-            
+
+            _addFile = new DelegateCommand(
+                () =>
+                {
+                    OpenFileDialog fileDialog = new OpenFileDialog();
+                    fileDialog.Multiselect = true;
+
+                    if (fileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        foreach (string pth in fileDialog.FileNames)
+                        {
+                            StandardFile temp = new StandardFile();
+                            temp.Path = pth;
+                            temp.Description = "";
+                            _selectedIssue.StandardFiles.Add(temp);
+                        }
+
+                        OnPropertyChanged("FileList");
+                    }
+                },
+                () => SelectedIssue != null);
+
+            _addIssue = new DelegateCommand(
+                () =>
+                {
+                    StandardIssue temp = new StandardIssue();
+                    temp.IsCurrent = false;
+                    temp.Issue = DateTime.Now.ToShortDateString();
+                    temp.Standard = _methodInstance.Standard;
+
+                    _methodInstance.Standard.StandardIssues.Add(temp);
+
+                    _issueList.Add(temp);
+
+                    SelectedIssue = temp;
+                });
+
             _addMeasurement = new DelegateCommand(
                 () => 
                 {
@@ -36,7 +76,31 @@ namespace Specifications.ViewModels
                     tempMea.UM = "";
                     Measurements.Add(tempMea);
                 });
-            
+
+            _openFile = new DelegateCommand(
+                () =>
+                {
+                    System.Diagnostics.Process.Start(_selectedFile.Path);
+                },
+                () => _selectedFile != null);
+
+            _removeFile = new DelegateCommand(
+                () =>
+                {
+                    _selectedIssue.StandardFiles.Remove(_selectedFile);
+                    SelectedFile = null;
+                },
+                () => _selectedFile != null);
+
+            _removeIssue = new DelegateCommand(
+                () =>
+                {
+                    _methodInstance.Standard.StandardIssues.Remove(_selectedIssue);
+                    _issueList.Remove(_selectedIssue);
+                    SelectedIssue = null;
+                },
+                () => _selectedIssue != null);
+
             _removeMeasurement = new DelegateCommand(
                 () => 
                 {
@@ -45,10 +109,29 @@ namespace Specifications.ViewModels
                 },
                 () => SelectedMeasurement != null );
         }
+
+        public DelegateCommand AddFileCommand
+        {
+            get { return _addFile; }
+        }
         
         public DelegateCommand AddMeasurementCommand
         {
             get { return _addMeasurement; }
+        }
+
+        public ObservableCollection<StandardIssue> IssueList
+        {
+            get
+            {
+                return _issueList;
+            }
+
+            private set
+            {
+                _issueList = value;
+                OnPropertyChanged("IssueList");
+            }
         }
 
         public Method MethodInstance
@@ -58,7 +141,12 @@ namespace Specifications.ViewModels
             {
                 _methodInstance = value;
                 Measurements = new ObservableCollection<MethodMeasurement>(_methodInstance.Measurements);
+                IssueList = new ObservableCollection<StandardIssue>(_methodInstance.Standard.StandardIssues);
                 OnPropertyChanged("Name");
+                OnPropertyChanged("Oem");
+                OnPropertyChanged("Property");
+                OnPropertyChanged("SpecificationList");
+                OnPropertyChanged("ResultList");
             }
         }
 
@@ -83,10 +171,58 @@ namespace Specifications.ViewModels
                 return (_methodInstance != null) ? _methodInstance.Standard.Name : null;
             }
         }
+
+        public string Oem
+        {
+            get
+            {
+                return (_methodInstance != null) ? _methodInstance.Standard.Organization.Name : null;
+            }
+        }
+
+        public DelegateCommand OpenFileCommand
+        {
+            get { return _openFile; }
+        }
+
+        public string Property
+        {
+            get
+            {
+                return (_methodInstance != null) ? _methodInstance.Property.Name : null;
+            }
+        }
+
+        public DelegateCommand RemoveFileCommand
+        {
+            get { return _removeFile; }
+        }
         
         public DelegateCommand RemoveMeasurementCommand
         {
             get { return _removeMeasurement; }
+        }
+
+        public List<Test> ResultList
+        {
+            get
+            {
+                if (_methodInstance == null)
+                    return null;
+                else
+                    return new List<Test>(_methodInstance.Tests); }
+        }
+
+        public StandardFile SelectedFile
+        {
+            get { return _selectedFile; }
+            set { _selectedFile = value; }
+        }
+
+        public StandardIssue SelectedIssue
+        {
+            get { return _selectedIssue; }
+            set { _selectedIssue = value; }
         }
         
         public MethodMeasurement SelectedMeasurement
@@ -100,6 +236,19 @@ namespace Specifications.ViewModels
                 _selectedMeasurement = value;
                 _removeMeasurement.RaiseCanExecuteChanged();
                 OnPropertyChanged("SelectedMeasurement");
+            }
+        }
+
+        public List<Specification> SpecificationList
+        {
+            get
+            {
+                if (_methodInstance == null)
+                    return null;
+                else
+                    return new List<Specification>(_methodInstance.Requirements
+                        .Select(req => (req.SpecificationVersions !=  null) 
+                            ? req.SpecificationVersions.Specification : null));
             }
         }
         
