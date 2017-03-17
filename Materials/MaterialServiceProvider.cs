5793299bc1410e18dbfbe4411ecc731392b25e2f
Pietro Simonini <pietro.simonini@hotmail.it>
2017-03-17 15:17:57 +0000

    Updated MaterialServiceProvider.cs

--- Materials/MaterialServiceProvider.cs
+++ Materials/MaterialServiceProvider.cs
@@ -37,29 +37,68 @@ namespace Materials
             return output;
         }
 
-        public Material CreateNewMaterial()
+        private Material GetMaterial()
         {
-            Material output;
+            if (target.Material != null)
+                return;
+
             MaterialCreationDialog matDialog = _container.Resolve<MaterialCreationDialog>();
             
             if (matDialog.ShowDialog() == true)
             {
-                output = _entities.Materials.First(mat => mat.ID == matDialog.ValidatedMaterial.ID);
-                if (output.Construction.Project == null)
-                {
-                    ProjectPickerDialog prjDialog = new ProjectPickerDialog(_entities);
-                    if (prjDialog.ShowDialog() == true)
-                        output.Construction.Project = prjDialog.ProjectInstance;
-                }
+                Construction tempConstruction = _entities.Constructions(con => con.Type.Code matDialog.MaterialType &&
+                                                                                con.Line == matDialog.MaterialLine &&
+                                                                                con.Aspect.Code == matDialog.MaterialAspect);
+                
+                Recipe tempRecipe = _entities.Recipes(rcp => rcp.Code == matDialog.MaterialRecipe);
 
-                if (output.Recipe.Colour == null)
-                    output.Recipe.Colour = PickColourForRecipe();
+                if (tempConstruction != null && tempRecipe != null)
+                    output = _entities.Materials.FirstOrDefault(mat => mat.ConstructionID == tempConstruction.ID &&
+                                                                                mat.RecipeID == tempRecipe.ID);
+
+                else 
+                {
+                    if (tempConstruction == null)
+                    {
+                        tempConstruction = new Construction();
+                        tempConstruction.Type = _entities.MaterialTypes.First(mty => mty.Code == matDialog.MaterialType);
+                        tempConstruction.Line = matDialog.MaterialLine;
+                        tempConstruction.Aspect = _entities.Aspects.FirstOrDefault(asp => asp.Code == matDialog.MaterialAspect);
+                    }
+
+                    if (tempRecipe == null)
+                    {
+                        tempRecipe = new Recipe();
+                        tempRecipe.Code == matDialog.MaterialRecipe;
+                    };
+                }
+                
+                if (output == null)
+                {
+                    output = new Material();
+                    output.Construction = tempConstruction;
+                    output.Recipe = tempRecipe;
+                }
 
                 return output;
             }
 
             else
-                return null;
+                return;
+        }
+
+        public void CheckMaterialData(Material target)
+        {
+            if (output.Construction.Project == null)
+            {
+                ProjectPickerDialog prjDialog = new ProjectPickerDialog(_entities);
+                if (prjDialog.ShowDialog() == true)
+                    output.Construction.Project = prjDialog.ProjectInstance;
+            }
+
+            if (output.Recipe.Colour == null)
+                output.Recipe.Colour = PickColourForRecipe();
+
         }
 
         public Batch GetBatch(string batchNumber)
