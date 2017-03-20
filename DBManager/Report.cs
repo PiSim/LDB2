18c0f3ef2bd4c8a8b42733d283b9584e72718bd7
Pietro Simonini <pietro.simonini@hotmail.it>
2017-03-20 08:45:33 +0000

    Updated Report.cs

--- DBManager/Report.cs
+++ DBManager/Report.cs
@@ -24,7 +24,7 @@ namespace DBManager
     
         public int ID { get; set; }
         public int AuthorID { get; private set; }
-        public int BatchID { get; private set; }
+        public int BatchID { get; set; }
         public string Category { get; set; }
         public string Description { get; set; }
         public Nullable<System.DateTime> EndDate { get; set; }
