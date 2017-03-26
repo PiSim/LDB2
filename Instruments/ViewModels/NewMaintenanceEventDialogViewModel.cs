5b70709129aa7fd8d6f4e5685138bbcc989df5f2
Pietro Simonini <pietro.simonini@hotmail.it>
2017-03-26 20:57:01 +0200

    Prepared NewMaintenanceEventDialog V and VM

--- Instruments/ViewModels/NewMaintenanceEventDialogViewModel.cs
+++ Instruments/ViewModels/NewMaintenanceEventDialogViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Mvvm;
+﻿using DBManager;
+using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,70 @@ namespace Instruments.ViewModels
 {
     public class NewMaintenanceEventDialogViewModel : BindableBase
     {
+        private DateTime _date;
+        private DBEntities _entities;
+        private DelegateCommand _cancel, _confirm;
+        private Person _selectedTech;
+        private string _description;
+        private Views.NewMaintenanceEventDialog _parentDialog;
+
+        public NewMaintenanceEventDialogViewModel(DBEntities entities,
+                                                Views.NewMaintenanceEventDialog parentDialog) : base()
+        {
+            _date = DateTime.Now.Date;
+            _entities = entities;
+            _parentDialog = parentDialog;
+
+            _cancel = new DelegateCommand(
+                () =>
+                {
+                    _parentDialog.DialogResult = false;
+                });
+
+            _confirm = new DelegateCommand(
+                () =>
+                {
+                    _parentDialog.DialogResult = true;
+                });
+        }
+
+        public DelegateCommand CancelCommand
+        {
+            get { return _cancel; }
+        }
+
+        public DelegateCommand ConfirmCommand
+        {
+            get { return _confirm; }
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set
+            {
+                _date = value;
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+            }
+        }
+
+        public List<Person> TechList
+        {
+            get { return new List<Person>(_entities.People.Where(per => per.Role == "TL")); }
+        }
+
+        public Person SelectedTech
+        {
+            get { return _selectedTech; }
+            set { _selectedTech = value; }
+        }
     }
 }
