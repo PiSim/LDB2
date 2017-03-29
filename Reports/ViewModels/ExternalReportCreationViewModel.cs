f068d1657ec1f1a7cc86382feaeed01412c204ef
Pietro Simonini <pietro.simonini@hotmail.it>
2017-03-29 11:56:23 +0000

    Updated ExternalReportCreationViewModel.cs

--- Reports/ViewModels/ExternalReportCreationViewModel.cs
+++ Reports/ViewModels/ExternalReportCreationViewModel.cs
@@ -67,7 +67,7 @@ namespace Reports.ViewModels
                     foreach (Batch selectedBatch in _batchList)
                     {
                         ExternalReportBatchMapping tempMap = new ExternalReportBatchMapping();
-                        tempMap.Batch = selectedBatch;
+                        tempMap.Batch = _entities.Batches[_selectedBatch.ID];
                         
                         output.BatchMappings.Add(tempMap);
                     }
