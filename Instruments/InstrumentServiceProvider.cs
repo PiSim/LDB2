f15b30800f39f9e0e54dd16cae814883c4ea3378
Pietro Simonini <pietro.simonini@hotmail.it>
2017-04-04 23:57:29 +0200

    Preliminary work on new branch

--- Instruments/InstrumentServiceProvider.cs
+++ Instruments/InstrumentServiceProvider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Instruments
 {
-    class InstrumentServiceProvider
+    public class InstrumentServiceProvider : IInstrumentServiceProvider
     {
     }
 }
