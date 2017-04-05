15ec85018d49a5e64f271baa4a811f48cde70693
Pietro Simonini <pietro.simonini@hotmail.it>
2017-04-05 06:54:05 +0000

    Updated InstrumentServiceProvider.cs

--- Instruments/InstrumentServiceProvider.cs
+++ Instruments/InstrumentServiceProvider.cs
@@ -1,4 +1,6 @@
-﻿using Infrastructure;
+﻿using DBManager;
+using Infrastructure;
+using Prism.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,21 @@ namespace Instruments
 {
     public class InstrumentServiceProvider : IInstrumentServiceProvider
     {
+        private DBEntities _entities;
+        private EventAggregator _eventAggregator;
+
+        public InstrumentServiceProvider(DBEntities entities,
+                                        EventAggregator aggregator)
+        {
+            _entities = entities;
+            _eventAggregator = aggregator;
+        }
+
+        public Instrument RegisterNewInstrument()
+        {
+
+        }
+
+
     }
 }
