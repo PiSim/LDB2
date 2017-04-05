2a8027fc8eec8841a9bf546e4f12f2cebc4f1d00
Pietro Simonini <pietro.simonini@hotmail.it>
2017-04-05 06:54:51 +0000

    Updated IInstrumentServiceProvider.cs

--- Infrastructure/IInstrumentServiceProvider.cs
+++ Infrastructure/IInstrumentServiceProvider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DBManager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Infrastructure
 {
     public interface IInstrumentServiceProvider
     {
+        Instrument RegisterNewInstrument();
     }
 }
