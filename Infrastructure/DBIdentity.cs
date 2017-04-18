445990b0f070387e804e1bc3604aa03716bc6db0
Pietro Simonini <pietro.simonini@hotmail.it>
2017-04-18 11:36:39 +0000

    Added file DBIdentity.cs

---
+++ Infrastructure/DBIdentity.cs
@@ -0,0 +1,54 @@
+using DBManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Security
+{
+    public class DBIdentity : IIdentity
+    {
+        private User _user;
+
+        public DBIdentity()
+        {
+
+        }
+
+        public DBIdentity(User authUser)
+        {
+            _user = authUser;
+        }
+
+        public string Name
+        {
+            get
+            {
+                if (_user == null)
+                    return null;
+
+                return _user.FullName ?? _user.UserName;
+            }
+        }
+
+        public string AuthenticationType
+        {
+            get { return "Custom authentication"; }
+        }
+
+        public bool IsAuthenticated
+        {
+            get
+            {
+                return _user != null;
+            }
+        }
+
+        public User User
+        {
+            get { return _user; }
+        }
+    }
+}
