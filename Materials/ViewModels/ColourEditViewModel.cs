b36cadb86bae5d00936a978cfcb506edbb96d886
Pietro Simonini <pietro.simonini@hotmail.it>
2017-05-02 08:41:52 +0000

    Updated ColourEditViewModel.cs

--- Materials/ViewModels/ColourEditViewModel.cs
+++ Materials/ViewModels/ColourEditViewModel.cs
@@ -11,10 +11,26 @@ namespace Materials.ViewModels
     public class ColourEditViewModel :BindableBase
     {
         private Colour _colourInstance;
+        private DBEntities _entities;
 
-        public ColourEditViewModel() : base()
+        public ColourEditViewModel(DBEntities entities) : base()
         {
+            _entities = entities;
+        }
 
+        public Colour ColourInstance
+        {
+            get { return _colourInstance; }
+            set
+            {
+                if (value != null)
+                    _colourInstance = _entities.Colours.First(clr => clr.ID == value.ID);
+                
+                else
+                    _colourInstance = value;
+                
+                RaisePropertyChanged("ColourInstance"); 
+            }
         }
     }
 }
