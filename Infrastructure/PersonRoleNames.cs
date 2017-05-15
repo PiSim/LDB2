b652b0bafb2ce7c73c99d8f109a99cba6c9968a9
Pietro Simonini <pietro.simonini@hotmail.it>
2017-05-15 10:18:51 +0000

    Updated PersonRoleNames.cs

--- Infrastructure/PersonRoleNames.cs
+++ Infrastructure/PersonRoleNames.cs
@@ -9,7 +9,7 @@ namespace Infrastructure
     public class PersonRoleNames
     {
         public static string CalibrationTech { get { return "CAL_TECH"; } }
-        public static string LabManager { get { return "LAB_MANAGER"; } };
+        public static string LabManager { get { return "LAB_MANAGER"; } }
         public static string MaterialTestingTech { get { return "TEST_TECH"; } }
         public static string ProjectLeader { get { return "PRJ_LEADER"; } }
     }
