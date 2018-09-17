e2107b17ecead8109c0ad5ded1f667e12637ee05
Pietro Simonini <pietro.simonini@hotmail.it>
2018-09-17 07:31:55 +0200

    cmd pattern base

---
+++ ClassLibrary1/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class DataService<T> where T : DBContext , IDataService
+    {
+        private IDBContextFactory<T> _contextFactory ;
+
+
+        public DataService(IDBContextFactory<T> contextFactory )
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public void Execute(ICommand commandObject)
+        {
+            commandObject.Execute(_contextFactory.Create());
+        }
+
+        public T2 RunQuery<T2>(IScalar<T2> queryObject)
+        {
+            return queryObject.Execute(_contextFactory.Create());
+        }
+
+        public IQueryable<T2> RunQuery<T2>(IQuery<T2> queryObject)
+        {
+            return queryObject.Execute(_contextFactory.Create());
+        }
+    }
+}
---
+++ ClassLibrary1/IDataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public interface IDataService
+    {
+        T RunQuery<T>(IScalar<T> queryObject);
+        IQueryable<T> RunQuery<T>(IQuery<T> queryObject);
+        void Execute(ICommand commandObject);
+    }
+}
